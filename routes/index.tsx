7fba40311e2baa850ac45a27be8a7df67c370c10
Patrick <patrick.fanroth@gmail.com>
2025-03-29 17:50:11 +0100

    delete unused import

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,3 @@
-import { useSignal } from "@preact/signals";
 import { parseNovelbuddy } from "../src/parseNovelbuddy.ts";
 // function import um die function parseNovelbuddy aufrufen zu können,
 // die function downloaded das html auf den server in der variablen als string
